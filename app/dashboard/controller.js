0adde8b1b1d2ab21f3eefe58ea73e5a553a37d78
sadamhusein77.sh <sadamhusein77.sh@gmail.com>
2025-04-09 14:58:13 +0700

    feat: [BE-000] [SADAM] add dashboard feat

--- app/dashboard/controller.js
+++ app/dashboard/controller.js
@@ -1,7 +1,23 @@
+const Transaction = require('../transaction/model');
+const Voucher = require('../voucher/model');
+const Category = require('../category/model');
+const Player = require('../player/model');
+
 module.exports = {
     index: async (req, res) => {
         try {
-            res.render('index', {
+            const transactions = await Transaction.countDocuments();
+            const players = await Player.countDocuments();
+            const vouchers = await Voucher.countDocuments();
+            const categories = await Category.countDocuments();
+
+            res.render('admin/dashboard/view_dashboard', {
+                count: {
+                    transactions,
+                    vouchers,
+                    categories,
+                    players
+                },
                 name: req.session.user.name,
                 title: 'Dashboard'
             });
