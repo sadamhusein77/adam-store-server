40e79fa1c776091436a08607da1e53c84d87169e
sadamhusein77.sh <sadamhusein77.sh@gmail.com>
2025-04-14 11:11:26 +0700

    feat: [BE-000] [SADAM] add auth login

--- app/middleware/auth.js
+++ app/middleware/auth.js
@@ -1,3 +1,7 @@
+const config = require("../../config");
+const jwt = require("jsonwebtoken");
+const Player = require('../player/model');
+
 module.exports = {
     isLoginAdmin: (req, res, next) => {
         if(!req.session.user) {
@@ -7,5 +11,24 @@ module.exports = {
         }
 
         next();
+    },
+    isLoginPlayer: async (req, res, next) => {
+        try {
+            const token = req.headers.authorization ? req.headers.authorization.replace('Bearer ', '') : null
+
+            const data = jwt.verify(token, config.jwtKey)
+
+            const player = await Player.findOne({ _id: data.player.id })
+
+            if(!player) throw new Error();
+
+            req.player = player
+            req.token = token
+            next()
+        } catch (error) {
+            res.status(401).json({
+                error: 'Not authorized to access this resources'
+            })
+        }
     }
 }
